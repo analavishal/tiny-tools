333da091b6fc63847aa329f559568d2faa77e71b
Vishal <rao.vishal10@gmail.com>
2017-06-06 21:25:14 +0530

    Battery Health for Windows 10 For better battery Life
    Python3.6
    
    Reminds to connect charger if battery less than 40%
    Reminds to remove charger if battery greater than 80%

---
+++ battery.py
@@ -0,0 +1,21 @@
+import wmi
+import winsound
+import ctypes
+import time
+
+t = wmi.WMI(moniker = "//./root/wmi")
+b = t.ExecQuery('Select * from BatteryFullChargedCapacity')
+total = b[0].FullChargedCapacity
+b = t.ExecQuery('Select * from BatteryStatus where Voltage > 0')
+charge = round((b[0].RemainingCapacity / total) * 100)
+chargerConnected = b[0].PowerOnLine
+while True:
+    if charge <= 40 and not chargerConnected:
+        winsound.PlaySound('sound.wav', winsound.SND_FILENAME)
+        ctypes.windll.user32.MessageBoxW(0, "Connect charger", "Battery Health", 1)
+    elif charge >= 80 and chargerConnected:
+        winsound.PlaySound('sound.wav', winsound.SND_FILENAME)
+        ctypes.windll.user32.MessageBoxW(0, "Remove charger", "Battery Health", 1)
+    time.sleep((total - charge)*60)
+    charge = round((b[0].RemainingCapacity / total) * 100)
+    chargerConnected = b[0].PowerOnLine
