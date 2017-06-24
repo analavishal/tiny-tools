ec249e4c30560256e096e3467d7cb2f6c6d0fa47
analavishal <rao.vishal10@gmail.com>
2017-06-24 13:44:23 +0530

    Update battery.py

--- battery.py
+++ battery.py
@@ -5,10 +5,10 @@ import time
 t = wmi.WMI(moniker = "//./root/wmi")
 b = t.ExecQuery('Select * from BatteryFullChargedCapacity')
 total = b[0].FullChargedCapacity
-b = t.ExecQuery('Select * from BatteryStatus where Voltage > 0')
-charge = round((b[0].RemainingCapacity / total) * 100)
-chargerConnected = b[0].PowerOnLine
 while True:
+    b = t.ExecQuery('Select * from BatteryStatus where Voltage > 0')
+    charge = round((b[0].RemainingCapacity / total) * 100)
+    chargerConnected = b[0].PowerOnLine
     if charge <= 40 and not chargerConnected:
         winsound.PlaySound('sound.wav', winsound.SND_FILENAME)
         ctypes.windll.user32.MessageBoxW(0, "Connect charger", "Battery Health", 1)
@@ -16,6 +16,4 @@ while True:
         winsound.PlaySound('sound.wav', winsound.SND_FILENAME)
         ctypes.windll.user32.MessageBoxW(0, "Remove charger", "Battery Health", 1)
     time.sleep(2*60)
-    b = t.ExecQuery('Select * from BatteryStatus where Voltage > 0')
-    charge = round((b[0].RemainingCapacity / total) * 100)
-    chargerConnected = b[0].PowerOnLine
+    
